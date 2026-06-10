4c1bddd8680e0218e87504d60fd278c712847908
Ross Golder <ross@golder.org>
2026-06-10 12:32:54 +0700

    fix: regenerate CRDs with current controller-gen

--- apis/sharedlink/v1beta1/zz_generated.deepcopy.go
+++ apis/sharedlink/v1beta1/zz_generated.deepcopy.go
@@ -21,7 +21,7 @@ limitations under the License.
 package v1beta1
 
 import (
-	v1 "github.com/crossplane/crossplane/apis/v2/core/v2"
+	"github.com/crossplane/crossplane/apis/v2/core/v2"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 )
 
@@ -113,12 +113,12 @@ func (in *SharedLinkParameters) DeepCopyInto(out *SharedLinkParameters) {
 	}
 	if in.SiteDomainRef != nil {
 		in, out := &in.SiteDomainRef, &out.SiteDomainRef
-		*out = new(v1.Reference)
+		*out = new(v2.Reference)
 		(*in).DeepCopyInto(*out)
 	}
 	if in.SiteDomainSelector != nil {
 		in, out := &in.SiteDomainSelector, &out.SiteDomainSelector
-		*out = new(v1.Selector)
+		*out = new(v2.Selector)
 		(*in).DeepCopyInto(*out)
 	}
 	if in.Password != nil {
